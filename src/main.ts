677cae0f64ac648423e4f9acd70a878877eb3019
Nodir <y_nodir@yahoo.com>
2025-02-25 09:19:54 +0100

    Enabling CORS for allowing FE dev.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // TODO Allow only frontend origin
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Referral service')
