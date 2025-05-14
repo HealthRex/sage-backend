97c207513cd72dafe8c09d47cdfe23dae7e7f623
Nodir <y_nodir@yahoo.com>
2025-05-14 14:20:17 +0200

    Updating Pathway prompt as suggested. In `Clinical Notes` section using actual clinical notes instead of autopopulated template as it anyway seems hallucinated now, mostly. Added TODO to revert to autopopulated data after it's fixed - after that, prompt for `/ask-pathway*` endpoints should be updated as well as they're also using clinical notes instead of autopopulated data.

--- src/pathway/pathway.service.ts
+++ src/pathway/pathway.service.ts
@@ -9,6 +9,10 @@ import { AnswersResponse } from './models/answersResponse';
 import { Citation, SpecialistAIResponse } from '../models/specialistAIResponse';
 import EventSourceStream from '@server-sent-stream/web';
 import { ChatRequest } from '../models/chatRequest';
+import * as fs from 'node:fs';
+import { join } from 'path';
+
+const promptFilePath: string = './resources/pathway_prompt.md';
 
 @Injectable()
 export class PathwayService {
@@ -264,22 +268,14 @@ export class PathwayService {
   ): AnswersRequest {
     const request: AnswersRequest = new AnswersRequest([], shouldStream);
 
-    let combinedMessage =
-      'Clinical question: ' +
-      clinicalQuestion +
-      '\n Clinical notes: ' +
-      clinicalNotes +
-      '\n Additional information: ';
+    const promptTemplate: string = fs
+      .readFileSync(join(process.cwd(), promptFilePath))
+      .toString()
+      .replace('{{question}}', clinicalQuestion)
+      // TODO originally, this should be populated template, but since we're still not generating correct populated template, use clinical notes for now
+      .replace('{{notes}}', clinicalNotes);
 
-    for (const filledTemplateField of filledTemplate) {
-      combinedMessage +=
-        filledTemplateField['field'] +
-        ': ' +
-        filledTemplateField['value'] +
-        '\n';
-    }
-
-    request.messages.push(new Message('user', combinedMessage));
+    request.messages.push(new Message('user', promptTemplate));
 
     this.logger.debug('Pathway API request: ', request);
     return request;
