6a3817e58f91b8d3f16dc3151e6f7b80603b08c7
Nodir <y_nodir@yahoo.com>
2025-05-26 10:32:15 +0200

    Trying to fix cookies not being sent from FE: - setting secure=True with sameSite=none.

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
       resave: false,
       cookie: {
         sameSite: 'none',
-        secure: false,
+        secure: true,
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       },
       saveUninitialized: false,
