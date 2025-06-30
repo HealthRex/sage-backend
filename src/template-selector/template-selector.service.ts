ff1e521907449e4771453fd6d63f3cc43d9c0d2d
Nodir <y_nodir@yahoo.com>
2025-06-30 13:35:48 +0200

    Updated template matching logic after Embeddings API has switched to returning to JSON templates.

--- src/template-selector/template-selector.service.ts
+++ src/template-selector/template-selector.service.ts
@@ -8,53 +8,51 @@ import { plainToInstance } from 'class-transformer';
 
 // TODO would passing drive.google.com/file/.../view link work as well?
 const templateGoogleDocLinks: { [key: string]: string } = {
-  'Allergy eConsult Checklists _extracted.txt':
+  'allergy_.*\\.json':
     'https://docs.google.com/document/d/1JtFiX31BDZWt0zt0TMKvzLEUxZO5OYlO/edit',
-  'Cardiology eConsult Checklists_extracted.txt':
+  'cardiology_.*\\.json':
     'https://drive.google.com/file/d/1oHN8mjupLrk7QVoH-s2hNNM3V4qCM-Cx/view',
-  'Chemical Dependency eConsult Checklists_extracted.txt':
+  'chemical_dependency_.*\\.json':
     'https://drive.google.com/file/d/14Alx6GLZWsl5_D8c57geJSCdWitunPpr/view',
-  'Dermatology eConsult Checklists_extracted.txt':
+  'dermatology_.*\\.json':
     'https://drive.google.com/file/d/1Bc695IqIwT65sSz4uxUJraKfN3BxGVOG/view',
-  'Endocrinology eConsult Checklists FINAL 4.19.22_extracted.txt':
+  'endocrinology_.*\\.json':
     'https://docs.google.com/document/d/1tA1bkGYlnoFq2OXKMfP996ZFGoSH0DRO/edit',
-  'ENT eConsult Checklists_extracted.txt':
+  'ent_.*\\.json':
     'https://drive.google.com/file/d/1Cof4fRSaH5HbMmr_O8mK3xrb-2YElpov/view',
-  'Gastroenterology eConsult Checklists_extracted.txt':
+  'gastroenterology_.*\\.json':
     'https://drive.google.com/file/d/1KzIx98EJlN8AGlHaYDBHQ0T4BEV9BvX4/view',
-  'Gynecology eConsult Checklists_extracted.txt':
+  'gynecology_.*\\.json':
     'https://drive.google.com/file/d/1iHzbpO-Kmm98z8XxMXtBd191tzluBv0e/view',
-  'Hematology eConsult Checklists FINAL 9.7.22_extracted.txt':
+  'hematology_.*\\.json':
     'https://drive.google.com/file/d/1nRdw1uIWAAw047snDVXfWGr6AwiwEEeW/view',
-  'Hepatology Oncology eConsult Checklists_extracted.txt':
+  'hepatology_.*\\.json':
     'https://drive.google.com/file/d/14ZSQwWhmqcLETp1nNNst2caPgtghQ4OP/view',
-  'Infectious Disease eConsult Checklists_3.13.24_extracted.txt':
+  'infectious_disease_.*\\.json':
     'https://drive.google.com/file/d/1W2eXxt3i2U6pdEOiydE4pJLfkZQmQ4qr/view',
-  'Interventional Pulmonology eConsult Checklists _extracted.txt':
+  'interventional_pulmonology_.*\\.json':
     'https://drive.google.com/file/d/1VCmKnX4wdh9rxE9DOQDxUhVRocZx9gjH/view',
-  'LGBTQ+ eConsult Checklists_extracted.txt':
+  'lgbtqplus_.*\\.json':
     'https://drive.google.com/file/d/1UdZrdqu4g7ZDelVJy80pcfZb9JwjQ8yq/view',
-  'Nephrology eConsult Checklists_extracted.txt':
+  'nephrology_.*\\.json':
     'https://drive.google.com/file/d/1MhBZ4JgUvHh1vMDsO1b4WXr14zFaaMTk/view',
-  'Neurology eConsult Checklists_extracted.txt':
+  'neurology_.*\\.json':
     'https://drive.google.com/file/d/1yRjPXD90c3aU5Oz7bjt1keQecIo0yEx5/view',
-  'Oncology trial eConsult Checklists_extracted.txt':
+  'oncology_trial_.*\\.json':
     'https://drive.google.com/file/d/1o8VVVuUgSZhLjwCzc-2u3Va0MWOAgFUD/view',
-  'Orthopedics eConsult Checklists_extracted.txt':
+  'orthopedics_.*\\.json':
     'https://drive.google.com/file/d/1rw0CvTxyExXlSVIhYW_BNCcLtrGu5Yh1/view',
-  'Pain Medicine eConsult Checklists_extracted.txt':
+  'pain_medicine_.*\\.json':
     'https://drive.google.com/file/d/1Sj-riOwZ9ow6JsUh1zvloyuAFl5pJV20/view',
-  'Psychiatry eConsult Checklists_extracted.txt':
+  'psychiatry_.*\\.json':
     'https://drive.google.com/file/d/1cnJ3MZFkt5UdSpXdLAGBRB6FnNz5tqGN/view',
-  'Pulmonology eConsult Checklists_extracted.txt':
+  'pulmonology_.*\\.json':
     'https://drive.google.com/file/d/11qKl1vkOIsFGxYTAuMU6Yo57mrjQqH56/view',
-  'Rheumatology eConsult Checklists_extracted.txt':
+  'rheumatology_.*\\.json':
     'https://drive.google.com/file/d/1sNB3Ls_RcYoBKjuV3YspAg9h1H30oSCB/view',
-  'SHC Referral Templates Checklists compiled DRAFT_extracted.txt':
-    'https://drive.google.com/file/d/1uQ7GfwsuHb5GVmb7aU0B_VLC_tTxJe7T/view',
-  'Sleep Medicine eConsult Checklists_extracted.txt':
+  'sleep_medicine_.*\\.json':
     'https://drive.google.com/file/d/1qdJh1mSNo8OPRsGtOpdkxh8vpeiPS1Pk/view',
-  'Urology eConsult Checklists_extracted.txt':
+  'urology_.*\\.json':
     'https://drive.google.com/file/d/1dX80rZgPpzZGMbhdjpqBsHhCJkM3NInS/view',
 };
 
@@ -109,6 +107,14 @@ export class TemplateSelectorService {
     );
     this.logger.debug('data', data);
 
-    return templateGoogleDocLinks[data.suggestedTemplate];
+    let templateLink: string = '';
+    for (const key of Object.keys(templateGoogleDocLinks)) {
+      if (data.suggestedTemplate.match(key)) {
+        this.logger.debug('matched template', key);
+        templateLink = templateGoogleDocLinks[key];
+        break;
+      }
+    }
+    return templateLink;
   }
 }
