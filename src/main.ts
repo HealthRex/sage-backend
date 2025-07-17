b04430b3c0ab1ff170f78f94c3bc2bb596387b27
Nodir <y_nodir@yahoo.com>
2025-07-17 11:02:54 +0200

    Trying to fix localhost cookies for Chrome - added `partitioned: true`.

--- src/main.ts
+++ src/main.ts
@@ -43,6 +43,7 @@ async function bootstrap() {
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         sameSite: 'none',
         secure: true,
+        partitioned: true,
       },
       saveUninitialized: false,
     }),
